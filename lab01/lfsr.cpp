b0d260f8e494cc568abc329240e7afa98c3af133
Alex Yatsyna <yatsyna.firebalde@mail.ru>
2021-03-10 00:27:22 +0300

    Completed 1 lab

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -2,6 +2,6 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    *reg=( ( ( (*reg>>0) & 1) ^ ((*reg>>2) & 1) ^ ((*reg>>3) & 1) ^ ((*reg>>5)&1)) <<15) | (*reg>>1);
 }
 
--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -2,5 +2,38 @@
 
 bool ll_has_cycle(node *head) {
     /* Ваш код должен быть написан только внутри этой функции */
+    node *head2 = head;
+    bool isFirst=true;
+    if (head == nullptr)
+    {
+        return false;
+    }
+    while (true)
+    {
+        if(head == head2 && !isFirst )
+        {
+            break;
+        }
+        else
+        {
+            if(head->next != nullptr)
+            {
+            head = head->next;
+            }
+            for(int i=0;i<2;i++)
+            {
+                if (head2->next == nullptr)
+                {
+                    return false;
+                }
+                else 
+                {
+                    head2=head2->next;
+                }
+            }
+        }
+        isFirst=false;
+        
+    }
     return true;
 }
